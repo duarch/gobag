8c4fab2d066f305f66638bebe55d862c1c0527e1
Andre Duarte <andre.bh@hotmail.com>
2022-02-27 17:49:43 -0300

    Postgres conection

--- main.go
+++ main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
+	"os"
 	"text/template"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+func getEnvVars() {
+	err := godotenv.Load("credentials.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func conectDB() *sql.DB {
+	getEnvVars()
+	dbName := os.Getenv("DB_NAME")
+	dbSecret := os.Getenv("DB_KEY")
+	openKey := "user=postgres password=" + dbSecret + " dbname=" + dbName + " host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", openKey)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
 
 type Produto struct {
 	Nome, Descricao string
 	Preco           float64
-	Quantidade      int
+	id, Quantidade  int
 }
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
@@ -19,11 +44,30 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{"Camiseta", "Azul", 39, 1},
-		{"Calça", "Vermelha", 79, 3},
-		{"Camiseta", "Verde", 29, 5},
-		{"Camiseta", "Amarela", 19, 2},
+	db := conectDB()
+	selectProdutos, err := db.Query("SELECT * FROM produtos")
+	if err != nil {
+		panic(err)
+	}
+	p := Produto{}
+	produtos := []Produto{}
+	for selectProdutos.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = selectProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err)
+		}
+		// p.id = id
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+		produtos = append(produtos, p)
+
 	}
 	temp.ExecuteTemplate(w, "Index", produtos)
+	defer db.Close()
 }
