0ae6000600d77c4669f220a379c9f63798b2a47d
duarch <@users.noreply.github.com>
2024-01-29 08:08:44 -0300

    bootstrap table

--- src/app/components/mat-table/mat-table.component.ts
+++ src/app/components/mat-table/mat-table.component.ts
@@ -1,62 +1,29 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-mat-table',
   standalone: true,
-  imports: [],
-  template: `
-  <table mat-table [dataSource]="items" class="mat-elevation-z8">
-    <!-- ID Column -->
-    <ng-container matColumnDef="id">
-      <th mat-header-cell *matHeaderCellDef> ID </th>
-      <td mat-cell *matCellDef="let item"> {{item.id}} </td>
-    </ng-container>
-
-    <!-- Nome Column -->
-    <ng-container matColumnDef="nome">
-      <th mat-header-cell *matHeaderCellDef> Nome </th>
-      <td mat-cell *matCellDef="let item"> {{item.nome}} </td>
-    </ng-container>
-
-    <!-- Descrição Column -->
-    <ng-container matColumnDef="descricao">
-      <th mat-header-cell *matHeaderCellDef> Descrição </th>
-      <td mat-cell *matCellDef="let item"> {{item.descricao}} </td>
-    </ng-container>
-
-    <!-- Peso Column -->
-    <ng-container matColumnDef="peso">
-      <th mat-header-cell *matHeaderCellDef> Peso </th>
-      <td mat-cell *matCellDef="let item"> {{item.peso}} </td>
-    </ng-container>
-
-    <!-- Medidas Column -->
-    <ng-container matColumnDef="medidas">
-      <th mat-header-cell *matHeaderCellDef> Medidas </th>
-      <td mat-cell *matCellDef="let item"> {{item.medidas}} </td>
-    </ng-container>
-
-    <!-- Data de validade Column -->
-    <ng-container matColumnDef="dataValidade">
-      <th mat-header-cell *matHeaderCellDef> Data de validade </th>
-      <td mat-cell *matCellDef="let item"> {{item.dataValidade}} </td>
-    </ng-container>
-
-    <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
-    <tr mat-row *matRowDef="let row; columns: displayedColumns;"></tr>
-  </table>
-`,
+  imports: [NgFor],
+  templateUrl: './mat-table.component.html',
   styleUrl: './mat-table.component.css'
 })
+
+
+
 export class MatTableComponent {
-  items = ELEMENT_DATA;
-  displayedColumns: string[] = ['id', 'nome', 'descricao', 'peso', 'medidas', 'dataValidade'];
+  dados = [
+    { id: 1, nome: 'Kit de primeiros socorros', descricao: 'Contém itens essenciais de primeiros socorros', peso: '1kg', medidas: '20cm x 10cm x 10cm', dataValidade: '2025-12-31' },
+    { id: 2, nome: 'Água potável', descricao: 'Garrafas de água potável', peso: '500g', medidas: '30cm x 10cm x 10cm', dataValidade: '2025-12-31' },
+    { id: 3, nome: 'Comida enlatada', descricao: 'Alimentos não perecíveis', peso: '1kg', medidas: '15cm x 10cm x 10cm', dataValidade: '2025-12-31' },
+    { id: 4, nome: 'Lanterna', descricao: 'Lanterna alimentada por manivela', peso: '250g', medidas: '15cm x 5cm x 5cm', dataValidade: 'N/A' },
+    { id: 5, nome: 'Cobertor de emergência', descricao: 'Cobertor térmico para manter o calor corporal', peso: '150g', medidas: '10cm x 5cm x 5cm', dataValidade: 'N/A' },
+    // Adicione mais dados aqui conforme necessário
+  ];
+  
 }
 
-
-const ELEMENT_DATA = [
-  // Adicione seus dados aqui. Por exemplo:
-  {id: 1, nome: 'Item 1', descricao: 'Descrição 1', peso: 1.0079, medidas: 'Medida 1', dataValidade: '20/12/2023'},
-];
 
 
